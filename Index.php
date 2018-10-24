0a6a05a1502c71ab445d3101cc876306b8dd1c8e
nhatnam0603 <32708196+nhatnam0603@users.noreply.github.com>
2018-10-24 18:21:46 +0700

    index page first

---
+++ Index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Vy Domain</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="mystyle.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="header">
+    <div class="header1">
+      <div class="container">
+        <div class="row">
+          <div class="logo col-md-3 col-sm-3">
+              <a class="" href="Homepage.php"><img src="logo_trans.png" style="width:150px;height:150px"></a>
+          </div>
+          <div class="search-box col-md-6 col-sm-6"> 
+              <form class="search-form"> <input class="form-control" placeholder="Son, Nước hoa,..." type="text"> 
+                  <button class="btn btn-link search-btn"> <i class="glyphicon glyphicon-search"></i> 
+                  </button>                 
+              </form> 
+          </div>   
+          <div class="headerR col-md-3 col-sm-3">
+              <ul class="list-inline">            
+                  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+                  <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header2">
+      <div class="container">
+        <nav class="navigation">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+              </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li><a href="#">HOMEPAGE</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">SKIN CARE
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="Homepage.php">A</a></li>
+                            <li><a href="Homepage.php">B</a></li>
+                            <li><a href="Homepage.php">C</a></li>
+                            <li><a href="Homepage.php">D</a></li>
+                        </ul>
+                </li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">PERFUME</a></li>
+                <li><a href="#">PROMOTION</a></li>
+                <li><a href="#">ABOUT US</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+    <div class="container">    
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4"> 
+          <div class="panel panel-danger">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4"> 
+          <div class="panel panel-success">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+      </div>
+    </div><br>
+
+    <div class="container">    
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4"> 
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4"> 
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+      </div>
+    </div><br><br>
+
+    <footer class="container-fluid text-center">
+      <p>Online Store Copyright</p>  
+      <form class="form-inline">Get deals:
+        <input type="email" class="form-control" size="50" placeholder="Email Address">
+        <button type="button" class="btn btn-danger">Sign Up</button>
+      </form>
+    </footer>
+
+</body>
+</html>
